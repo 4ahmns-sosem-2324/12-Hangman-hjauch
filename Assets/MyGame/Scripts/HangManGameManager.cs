99074f8184493f2c69b71e9744f013840e6a84c5
henrik0314 <heja.1709@gmail.com>
2024-03-19 14:07:15 +0100

    Grundskript

--- Assets/MyGame/Scripts/HangManGameManager.cs
+++ Assets/MyGame/Scripts/HangManGameManager.cs
@@ -1,78 +1,110 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using Random = UnityEngine.Random;
 
-public class HangmanGameManager : MonoBehaviour
+public class HangmanGame : MonoBehaviour
 {
     public Text wordDisplayText;
+    public Text guessedLettersText;
     public Text attemptsText;
 
     private string[] words = { "apple", "banana", "orange", "grape", "strawberry", "kiwi", "peach" };
     private string selectedWord;
-    private List<char> guessedLetters = new List<char>();
-    private int attempts = 6;
+    private HashSet<char> wrongLetters = new HashSet<char>();
+    private HashSet<char> correctLetters = new HashSet<char>();
+    private int attempts = 5;
+    public GameObject [] hangman;
 
     void Start()
     {
         StartGame();
     }
 
-    public void StartGame()
+    void StartGame()
     {
         selectedWord = words[Random.Range(0, words.Length)];
-        guessedLetters.Clear();
-        attempts = 6;
+        wrongLetters.Clear();
+        correctLetters.Clear();
+        attempts = 5;
         UpdateDisplay();
     }
 
     void UpdateDisplay()
     {
-        wordDisplayText.text = GetDisplayWord();
-        attemptsText.text = "Attempts left: " + attempts;
-    }
-
-    string GetDisplayWord()
-    {
         string displayWord = "";
         foreach (char letter in selectedWord)
         {
-            if (guessedLetters.Contains(letter))
-                displayWord += letter;
+            if (correctLetters.Contains(letter))
+                displayWord += letter + " ";
             else
-                displayWord += "_";
+                displayWord += "_ ";
         }
-        return displayWord;
+        wordDisplayText.text = displayWord;
+
+        guessedLettersText.text = "Wrong Letters: ";
+        foreach (char letter in wrongLetters)
+        {
+            guessedLettersText.text += letter + ", ";
+        }
+
+        attemptsText.text = "Attempts left: " + attempts;
     }
 
-    public bool GuessLetter(char letter)
+    void Update()
     {
-       
-        if (guessedLetters.Contains(letter))
-            return false; // Letter already guessed
-
-        guessedLetters.Add(letter);
-
-        if (!selectedWord.Contains(letter))
+        if (Input.anyKeyDown && attempts > 0 && !CheckWin())
         {
-            attempts--;
-            if (attempts <= 0)
+            char keyPressed = GetKeyPressed();
+            if (keyPressed != '\0')
             {
-                Debug.Log("Sorry, you lost. The word was: " + selectedWord);
-                return false; // Game over
+                if (selectedWord.Contains(keyPressed))
+                {
+                    correctLetters.Add(keyPressed);
+                }
+                else if (!wrongLetters.Contains(keyPressed))
+                {
+                    wrongLetters.Add(keyPressed);
+
+                    attempts--;
+                }
+
+                UpdateDisplay();
+
+                if (attempts == 0)
+                {
+                    Debug.Log("Sorry, you lost. The word was: " + selectedWord);
+                }
+                else if (CheckWin())
+                {
+                    Debug.Log("Congratulations! You guessed the word: " + selectedWord);
+                }
             }
         }
+    }
 
-        if (GetDisplayWord().Equals(selectedWord))
+    char GetKeyPressed()
+    {
+        foreach (char c in Input.inputString)
         {
-            Debug.Log("Congratulations! You guessed the word: " + selectedWord);
-            UpdateDisplay();
-            return true; // Player won
+            if (char.IsLetter(c))
+            {
+                return char.ToLower(c);
+            }
         }
+        return '\0';
+    }
 
-        UpdateDisplay();
-        return true; // Guess successful
-
-
+    bool CheckWin()
+    {
+        foreach (char letter in selectedWord)
+        {
+            if (!correctLetters.Contains(letter))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
